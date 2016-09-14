3547c960483d9b4653ab0a399f750261c81b0923
Лабанков Юрий <yulabankov@truenorth.pro>
2016-09-14 10:44:05 +0300

    first commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Yulabankov
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String firstCommit = "This is first commit";
+        System.out.println(firstCommit);
+    }
+}
---
+++ src/TestHelper.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Yulabankov
+ */
+public class TestHelper {
+
+    public static String changeString(String text) {
+        return text + " !Add Changes!";
+    }
+
+}
